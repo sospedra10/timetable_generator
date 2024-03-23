1d9a782404d8edc11c40e541508070e3080f9df4
javiersospedralegarda <javiersospedralegarda@gmail.com>
2024-03-23 01:28:11 +0100

    Added streamlit features to show infomration

--- main2.py
+++ main2.py
@@ -7,7 +7,7 @@ from utils import generate_timetable, create_timetable_dataframe, get_employee_v
 st.set_page_config(layout="wide")
 st.title('Employee Timetable Generator')
 # select days for which to generate timetable
-days = st.slider('Select number of days to generate timetable:', 1, 30, 1)
+days = st.slider('Select number of days to generate timetable:', 1, 30, value=7, step=1)
 
 
 # Load data from Excel
@@ -19,14 +19,29 @@ vacation_data = pd.read_excel(f'{folder_data}/employee_data.xlsx', sheet_name='V
 # Employees sheet: 'Name'
 # Vacation sheet: 'Name', 'Vacation Start', 'Vacation End'
 
+
 # List to store all employees
 employees = list(employee_data['Name'])
+with st.expander("View Employees"):
+    st.write("#### Employees:")
+    st.write(', '.join(employees))
+
 
 # Get vacation dates for each employee
 employee_vacations = get_employee_vacations(employees, vacation_data)
 
+with st.expander("View Employee Vacations"):
+    st.write("#### Employee Vacations:")
+    for employee, vacations in employee_vacations.items():
+        st.write(f"**{employee}**: {', '.join([str(vacation)[:10] for vacation in vacations])}")
+
+    # Plot vacation dates per employee
+    st.write("#### Vacation Dates:")
+    st.bar_chart({employee: len(vacations) for employee, vacations in employee_vacations.items()})
+    
+
 # Generating timetables for each day
-employee_timetables = generate_timetable(days=7, employees=employees, employee_vacations=employee_vacations)
+employee_timetables = generate_timetable(days=days, employees=employees, employee_vacations=employee_vacations)
 
 # Output timetable
 print("Timetable:")
@@ -36,6 +51,22 @@ for date, timetable in employee_timetables.items():
         print(f"Shift: {entry[2]}, Employee: {entry[1]}")
 
 
-timetable_data = create_timetable_dataframe(employee_timetables, folder_data=folder_data)
+tabular_data, timetable_data = create_timetable_dataframe(employee_timetables, folder_data=folder_data)
     
-st.dataframe(timetable_data)
+
+with st.expander("View Timetable"):
+    st.write("#### Timetable:")
+    st.dataframe(timetable_data)
+
+# count number of shifts per employee and per shift
+shift_counts = tabular_data.groupby(['Employee', 'Shift']).size().unstack().fillna(0)
+# shift_counts = shift_counts[['Employee', 'Morning', 'Afternoon']]
+
+with st.expander("View Shift Counts"):
+    st.write("#### Shift Counts:")
+    st.write(shift_counts)
+
+    # Plot shift counts
+    st.bar_chart(shift_counts)
+
+
