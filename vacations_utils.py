9506b3cdb168e3b4204d7ee9a63c6d43d89edbbc
javiersospedralegarda <javiersospedralegarda@gmail.com>
2024-05-24 17:43:53 +0200

    Added function to check vacations collisions when adding new ones

--- vacations_utils.py
+++ vacations_utils.py
@@ -2,6 +2,25 @@ from streamlit import experimental_dialog
 import pandas as pd
 
 
+def are_vacation_collisions(employee_vacations, new_vacation_start, new_vacation_end):
+    "Check if the new vacation dates are colliding with other vacations."
+    for _, employee_vacation in employee_vacations.iterrows():
+        print()
+        print(employee_vacation)
+        print()
+        print(new_vacation_start, new_vacation_end)
+        print()
+        
+        print()
+        # Checking if the new vacation is inside the range of the employee vacation
+        if (employee_vacation['Vacation Start'] <= new_vacation_start <= employee_vacation['Vacation End']) | (employee_vacation['Vacation Start'] <= new_vacation_end <= employee_vacation['Vacation End']):
+            return True
+        # Checking if the vacation is inside the range of the new vacation
+        if (new_vacation_start <= employee_vacation['Vacation Start'] <= new_vacation_end) | (new_vacation_start <= employee_vacation['Vacation End'] <= new_vacation_end):
+            return True
+    return False
+
+
 @experimental_dialog("Add employee vacation:")
 def add_employee_vacation(st, vacation_data, folder_data):
     "Dialog to add a new employee vacation to the system."
@@ -15,14 +34,20 @@ def add_employee_vacation(st, vacation_data, folder_data):
     else:
         st.write("**No vacations for this employee at the moment**")
 
+    # if new_vacation_start > new_vacation_end:
+    #     st.warning("Vacation start date must be before vacation end date")
+
     # TODO: Add validation to check if the vacation dates are valid
     # TODO: Add validation to check if the employee already has a vacation
 
     if st.button('Submit'):
-        if new_employee_name in vacation_data['Name'].values:
+        # Check if the new employee vacations are not collapsing with other vacations
+        # if (employee_vacations['Vacation Start'] <= new_vacation_start <= employee_vacations['Vacation End']) | (employee_vacations['Vacation End'] >= new_vacation_start)):
+        # if (employee_vacations['Vacation Start'] <= new_vacation_start <= employee_vacations['Vacation End']).any():
+        if are_vacation_collisions(employee_vacations, new_vacation_start, new_vacation_end):
             st.error(f"Employee {new_employee_name} already has a vacation")
             return
-        if new_vacation_start >= new_vacation_end:
+        if new_vacation_start > new_vacation_end:
             st.error("Vacation start date must be before vacation end date")
             return
         new_vacation = pd.DataFrame({'Name': new_employee_name, 'Vacation Start': new_vacation_start, 'Vacation End': new_vacation_end}, index=[0])
