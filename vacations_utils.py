3756652f11e7865c61cbc55238afde40ae5f0d31
javiersospedralegarda <javiersospedralegarda@gmail.com>
2024-05-30 20:41:32 +0200

    2 columns for dates and finished adding vacations

--- vacations_utils.py
+++ vacations_utils.py
@@ -1,5 +1,6 @@
 from streamlit import experimental_dialog
 import pandas as pd
+from utils import save_data
 
 
 def are_vacation_collisions(employee_vacations, new_vacation_start, new_vacation_end):
@@ -18,13 +19,6 @@ def are_vacation_collisions(employee_vacations, new_vacation_start, new_vacation
     """
 
     for _, employee_vacation in employee_vacations.iterrows():
-        print()
-        print(employee_vacation)
-        print()
-        print(new_vacation_start, new_vacation_end)
-        print()
-        
-        print()
         # Checking if the new vacation is inside the range of the employee vacation
         if (employee_vacation['Vacation Start'] <= new_vacation_start <= employee_vacation['Vacation End']) | (employee_vacation['Vacation Start'] <= new_vacation_end <= employee_vacation['Vacation End']):
             return True
@@ -38,8 +32,9 @@ def are_vacation_collisions(employee_vacations, new_vacation_start, new_vacation
 def add_employee_vacation(st, vacation_data, folder_data):
     "Dialog to add a new employee vacation to the system."
     new_employee_name = st.selectbox('**Name:**', st.session_state.employees)
-    new_vacation_start = st.date_input('**Vacation Start:**')
-    new_vacation_end = st.date_input('**Vacation End:**')
+    new_vacation_start_col, new_vacation_end_col = st.columns(2)
+    new_vacation_start = new_vacation_start_col.date_input('**Vacation Start:**')
+    new_vacation_end = new_vacation_end_col.date_input('**Vacation End:**')
 
     employee_vacations = vacation_data[vacation_data['Name'] == new_employee_name]
     if employee_vacations.shape[0] > 0:
@@ -65,8 +60,8 @@ def add_employee_vacation(st, vacation_data, folder_data):
 
         st.write(vacation_data)
 
-        # save_data(folder_data, st.session_state.employees_estancos, vacation_data, timetable_data=None)
-        # st.rerun()
+        save_data(folder_data, st.session_state.employees_estancos, vacation_data, timetable_data=None)
+        st.rerun()
 
 @experimental_dialog("Edit employee vacation:")
 def edit_employee_vacation(st, vacation_data, folder_data):
